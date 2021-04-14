4e8ba6d7bc87cda6384b370a2f38948316b52f4e
Daniel <dtapiaramirez887@gmail.com>
2021-04-13 21:16:29 -0500

    commit

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ class MiNav extends HTMLElement {
       `<a href="index.html">
         <span
           class="material-icons">
-          perm_identify
+          perm_identity
         </span>
         Inicio
       </a>
